fed17439b75d14e31d721c204e8521da017c7ba4
tnar8152 <77207056+tnar8152@users.noreply.github.com>
2024-12-15 15:25:01 -0800

    ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package main.java;
+
 
 public class ClockDriver
 {
@@ -40,5 +42,87 @@ public class ClockDriver
         //  * Tick test for 11:59PM to 12:00AM
         //  * Tick test for 11:59AM to 12:00PM
         //  * Tick test for 12:59PM to 01:00PM
+        
+        ClockDisplay clockDisplay = new ClockDisplay();
+        clockDisplay.setTime(00,00);
+        System.out.println("Read Time " + clockDisplay.getTime());
+        clockDisplay.timeTick();
+        System.out.println("Read Time " + clockDisplay.getTime()); 
+        
+        ClockDisplay clockDisplay1 = new ClockDisplay(03,32);  
+        clockDisplay1.setTime(03,32);
+        System.out.println("Read Time " + clockDisplay1.getTime());
+        clockDisplay1.timeTick();
+        System.out.println("After Tick Read Time " + clockDisplay1.getTime());
+        
+        clockDisplay1.setTime(03,9);
+        System.out.println("Read Time " + clockDisplay1.getTime());
+        clockDisplay1.timeTick();
+        System.out.println("After Tick Read Time " + clockDisplay1.getTime());
+        
+        clockDisplay1.setTime(01,59);
+        System.out.println("Read Time " + clockDisplay1.getTime());
+        clockDisplay1.timeTick();
+        System.out.println("After Tick Read Time " + clockDisplay1.getTime());
+        
+        clockDisplay1.setTime(9,59);
+        System.out.println("Read Time " + clockDisplay1.getTime());
+        clockDisplay1.timeTick();
+        System.out.println("After Tick Read Time " + clockDisplay1.getTime());
+        
+        clockDisplay1.setTime(23,59);
+        System.out.println("Read Time " + clockDisplay1.getTime());
+        clockDisplay1.timeTick();
+        System.out.println("After Tick Read Time " + clockDisplay1.getTime());
+        
+        ClockDisplaySeconds clockDisplaySeconds = new ClockDisplaySeconds();
+        clockDisplaySeconds.setTime(00,00,00);
+        System.out.println("Read Time " + clockDisplaySeconds.getTime());
+        clockDisplaySeconds.timeTick();
+        System.out.println("Read Time " + clockDisplaySeconds.getTime()); 
+           
+        clockDisplaySeconds.setTime(3,32,59);
+        System.out.println("Read Time " + clockDisplaySeconds.getTime());
+        clockDisplaySeconds.timeTick();
+        System.out.println("Read Time " + clockDisplaySeconds.getTime()); 
+        clockDisplaySeconds.setTime(1,00,59);
+        System.out.println("Read Time " + clockDisplaySeconds.getTime());
+        clockDisplaySeconds.timeTick();
+        System.out.println("Read Time " + clockDisplaySeconds.getTime()); 
+        clockDisplaySeconds.setTime(1,59,59);
+        System.out.println("Read Time " + clockDisplaySeconds.getTime());
+        clockDisplaySeconds.timeTick();
+        System.out.println("Read Time " + clockDisplaySeconds.getTime()); 
+        clockDisplaySeconds.setTime(23,59,59);
+        System.out.println("Read Time " + clockDisplaySeconds.getTime());
+        clockDisplaySeconds.timeTick();
+        System.out.println("Read Time " + clockDisplaySeconds.getTime()); 
+        
+        ClockDisplay12Hour clockDisplay12Hour = new ClockDisplay12Hour();
+        clockDisplay12Hour.setTime(00,00);
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());
+        clockDisplay12Hour.timeTick();
+        System.out.println("Read Time " + clockDisplay12Hour.getTime()); 
+        
+        clockDisplay12Hour.setTime(3,32);
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());
+        clockDisplay12Hour.timeTick();
+        System.out.println("Read Time " + clockDisplay12Hour.getTime()); 
+        
+        clockDisplay12Hour.setTime(11,59);
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());
+        clockDisplay12Hour.timeTick();
+        System.out.println("Read Time " + clockDisplay12Hour.getTime()); 
+        
+        clockDisplay12Hour.setTime(11,59);
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());
+        clockDisplay12Hour.timeTick();
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());  
+        
+        clockDisplay12Hour.setTime(12,59);
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());
+        clockDisplay12Hour.timeTick();
+        System.out.println("Read Time " + clockDisplay12Hour.getTime());  
+        
     }
 }
