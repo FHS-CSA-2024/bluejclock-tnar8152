65870c8e717f10de173c38a9a4844c21e855775b
tnar8152 <77207056+tnar8152@users.noreply.github.com>
2024-12-15 15:20:42 -0800

    ClockDisplay.java

--- src/main/java/ClockDisplay.java
+++ src/main/java/ClockDisplay.java
@@ -1,3 +1,5 @@
+package main.java;
+
 
 public class ClockDisplay
 {
@@ -30,4 +32,44 @@ public class ClockDisplay
     //Implement a method updateDisplay that takes no parameters and returns nothing
     //The method should update the displayString with the current time in a format
     //  HH:MM
+    
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String display;
+    
+    public ClockDisplay() {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+    public ClockDisplay(int hour, int minute) {
+        
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        setTime(hour, minute);  
+        updateDisplay();
+    }
+    
+    public void timeTick() {
+        minutes.increment();
+        if(minutes.getValue() == 0) {
+            hours.increment();
+        }
+        updateDisplay();
+    }
+    
+    public void setTime(int hour, int minute) {
+        hours.setValue(hour);
+        minutes.setValue(minute);
+        updateDisplay();
+    }
+    
+    public String getTime() {
+        return display;
+    }
+    public void updateDisplay() {        
+        display = hours.getDisplayValue() + ":" +  minutes.getDisplayValue();        
+    }
+    
 }
