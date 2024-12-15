3414878e0b6f365b73274033dbe232333ca38804
tnar8152 <77207056+tnar8152@users.noreply.github.com>
2024-12-15 15:24:16 -0800

    ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,54 @@
+package main.java;
+
 
 public class ClockDisplaySeconds
 {
     //Extend ClockDisplaySeconds to include a seconds field.
     //Update all methods and parameters to accomodate this change
+    
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    private String display;
+    
+    public ClockDisplaySeconds() {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+    public ClockDisplaySeconds(int hour, int minute, int second) {
+        
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        setTime(hour, minute,second);  
+        updateDisplay();
+    }
+    
+    public void timeTick() {
+        seconds.increment();
+        if (seconds.getValue() == 0) {
+            minutes.increment();
+            if(minutes.getValue() == 0) {
+                hours.increment();
+            }
+        }
+            updateDisplay();
+    }
+    
+    public void setTime(int hour, int minute, int second) {
+        hours.setValue(hour);
+        minutes.setValue(minute);
+        seconds.setValue(second);
+        updateDisplay();
+    }
+    
+    public String getTime() {
+        return display;
+    }
+    public void updateDisplay() {        
+        display = hours.getDisplayValue() + ":" +  minutes.getDisplayValue() + ":" + seconds.getDisplayValue();        
+    }
 }
