ba58a0b746c5a56310f9839f961ee99b6e8af5f7
tnar8152 <77207056+tnar8152@users.noreply.github.com>
2024-12-15 15:22:17 -0800

    NumberDisplay.java

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -1,3 +1,5 @@
+package main.java;
+
 
 
 public class NumberDisplay
@@ -21,4 +23,45 @@ public class NumberDisplay
     //Implement a method increment that takes no parameters and returns nothing. 
     //  This function should increase value by 1, unless it reaches the limit.
     //  It should reset to 0 if the limit is reached
+    
+    private int limit;
+    private int value;
+    
+    public NumberDisplay(int rollOverLimit) {
+        limit = rollOverLimit;
+        value = 0;
+    }
+    
+    public int getLimit() {
+        return limit;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    public void setLimit( int tempLimit) {
+          limit = tempLimit;
+    }
+    public void setValue( int tempValue) {
+        if((tempValue >= 0) && (tempValue < limit)) {
+            value = tempValue;
+        }        
+    }
+    
+    public String getDisplayValue() {
+        
+        return String.format("%02d", value);
+        /** if (value < 10) {
+            return "0" + value;
+        } else {
+            return ""+ value;
+        } */
+    }
+    
+    public void increment() {
+        value = (value + 1) % limit;
+    }
+    
+    
+    
 }
